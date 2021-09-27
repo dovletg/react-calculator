fc25ae6dd79931ef725c9698caa939442cb9c55e
jahan <jahan@j-MacBook-Air.home>
2021-09-27 09:18:36 -0400

    created the button styled components

--- src/components/Calculator/Styled/index.js
+++ src/components/Calculator/Styled/index.js
@@ -35,3 +35,26 @@ export const Current = styled.div`
   color: white;
   font-size: 2.5rem;
 `
+
+export const Button = styled.button`
+  cursor: pointer;
+  font-size: 2rem;
+  border: 1px outset white;
+  outline: none;
+  background-color: rgba(255, 255, 255, 0.75);
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+
+  /* EXPENDED FORM EXAMPLE */
+  ${ function({ gridSpan }){
+    if(gridSpan){
+      return `grid-column: span ${gridSpan};`
+    }
+  }}
+
+  ${({operation}) => operation && `background-color: gray;`}
+  ${({control}) => control && `background-color: skyblue;`}
+  ${({equals}) => equals && `border-bottom-right-radius: 10px; background-color: gray;`}
+  ${({period}) => period && `border-bottom-left-radius: 10px; background-color: skyblue;`}
+`
