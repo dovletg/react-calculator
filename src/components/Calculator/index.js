2c3cb5787d040e35b6ad87cb5b6c5f08c4346ac7
jahan <jahan@j-MacBook-Air.home>
2021-10-01 23:54:56 -0400

    Handling All Clear and Delete Functionality

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -3,7 +3,9 @@ import { Container, Current, Previous, Screen, Button } from "./Styled";
 
 export default function Calculator() {
 
+  const [previous, setPrevious] = useState("")
   const [current, setCurrent] = useState("")
+  const [operation, setOperation] = useState("")
 
   const appendValue = (el) => {
     const value = el.target.getAttribute("data")
@@ -12,14 +14,24 @@ export default function Calculator() {
     setCurrent(current + value)
   }
 
+  const handleDelete = () => {
+    setCurrent(String(current).slice(0, -1))
+  }
+
+  const handleAllClear = () => {
+    setCurrent("")
+    setPrevious("")
+    setOperation("")
+  }
+
   return(
     <Container>
       <Screen>
-        <Previous>11 +</Previous>
+        <Previous>{previous} {operation}</Previous>
         <Current>{current}</Current>
       </Screen>
-      <Button gridSpan={2} control>AC</Button>
-      <Button control>DEL</Button>
+      <Button onClick={handleAllClear} gridSpan={2} control>AC</Button>
+      <Button onClick={handleDelete} control>DEL</Button>
       <Button operation>÷</Button>
       <Button data={'7'} onClick={appendValue}>7</Button>
       <Button data={'8'} onClick={appendValue}>8</Button>
